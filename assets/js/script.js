287ada76fe5f88e0ed05749f063420cc82c2a5dc
vivienne <vivienne.2214@yahoo.com>
2022-07-28 07:57:35 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {}
